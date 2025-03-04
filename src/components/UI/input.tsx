07423fe55ee0c12a205d4c86b32839815d974643
Aufa Azzahidi <aufa.azzahidi@gmail.com>
2025-03-05 00:48:00 +0700

    feat(talent-growth): changed color of input

--- src/components/UI/input.tsx
+++ src/components/UI/input.tsx
@@ -10,12 +10,12 @@ export default function Input({ placeholder, type, search }: InputProps) {
     return (
         <div className="flex items-center">
             <input
-                className="dark:bg-white dark:placeholder:text-gray-400 dark:text-black px-2 py-1 rounded-l-md"
+                className="dark:bg-neutral-600 dark:placeholder:text-neutral-400 dark:text-white px-2 py-1 rounded-l-md focus:outline-none"
                 type={type}
                 placeholder={placeholder}
             />
             {search && (
-                <button className="p-2 bg-white rounded-r-md dark:bg-white/10 hover:dark:bg-white/20 cursor-pointer transition-all ease-in-out duration-200">
+                <button className="p-2 bg-white rounded-r-md dark:bg-neutral-700 hover:dark:bg-white/20 cursor-pointer transition-all ease-in-out duration-200">
                     <FaSearch />
                 </button>
             )}
