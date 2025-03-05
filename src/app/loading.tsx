2c0b9f6ac449fb5b664f686b957bfc77323c0fcf
Aufa Azzahidi <aufa.azzahidi@gmail.com>
2025-03-05 23:26:45 +0700

    feat(talent-growth): add loading components for movies and enhance SkeletonCard structure

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import SkeletonCard from "@/components/loading";
+
+export default function Loading() {
+    return (
+        <div className="flex flex-col gap-4 items-center justify-start w-full animate-pulse">
+            <div className="flex w-full">
+                <div className="w-64 bg-neutral-600 h-10 rounded-md"></div>
+            </div>
+            <SkeletonCard />
+        </div>
+    );
+}
--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,10 +1,10 @@
-export default function Loading() {
+export default function SkeletonCard() {
     return (
         <div className="grid md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-4 w-full justify-between">
             {Array.from({ length: 10 }).map((_, index) => (
                 <div
                     key={index}
-                    className="flex flex-col items-center w-full h-86 gap-2 animate-pulse"
+                    className="flex flex-col items-center w-full h-86 gap-2"
                 >
                     <div className="w-full h-full relative rounded-lg overflow-hidden bg-neutral-600" />
                     <div className="w-full h-8 bg-neutral-600 rounded-md" />
