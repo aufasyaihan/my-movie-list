71819b98c594123c459af5ecc1cc7256ccbf1145
Aufa Azzahidi <aufa.azzahidi@gmail.com>
2025-03-07 14:23:53 +0700

    feat(talent-growth): update login page layout and improve error message display

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -29,9 +29,13 @@ export default function LoginPage() {
 
     return (
         <section className="w-full h-full flex flex-col justify-center items-center">
-            <div className="flex flex-col justify-around w-1/2 xl:w-1/3 h-fit xl:h-1/2 p-4 bg-white/10 backdrop-blur-md rounded-md shadow-lg gap-2">
-                <h1 className="text-3xl text-center font-bold">Welcome Back</h1>
-                {error && <p className="p-2 bg-red-200 text-red-500 border-l-4 w-full rounded-r-md">{error}</p>}
+            <div className="flex flex-col justify-around md:w-1/2 lg:w-1/3 h-fit xl:h-1/2 p-4 bg-white/10 backdrop-blur-md rounded-md shadow-lg gap-2">
+                <h1 className="text-3xl text-center font-bold">Let&apos;s Login</h1>
+                {error && (
+                    <p className="p-2 bg-red-200 text-red-500 border-l-4 w-full rounded-r-md">
+                        {error}
+                    </p>
+                )}
                 <div className="flex flex-col gap-2">
                     <Input
                         label="Email"
@@ -52,14 +56,24 @@ export default function LoginPage() {
                 </div>
                 <div className="flex items-center gap-2">
                     <hr className="w-full" />
-                    <button className="p-2 flex-11/12 bg-neutral-600 rounded-md font-bold hover:bg-neutral-700 transition-all ease-in-out duration-200 cursor-pointer" onClick={handleLogin}>
+                    <button
+                        className="p-2 flex-11/12 bg-neutral-600 rounded-md font-bold hover:bg-neutral-700 transition-all ease-in-out duration-200 cursor-pointer"
+                        onClick={handleLogin}
+                    >
                         Login
                     </button>
                     <hr className="w-full" />
                 </div>
-                <div className="flex gap-1 justify-center">
-                    <p>Don&apos;t have an account yet?</p>
-                    <Link className="text-amber-500 underline underline-offset-2 decoration-amber-500" href={"/register"}>Register Now</Link>
+                <div className="flex justify-center">
+                    <p>
+                        Don&apos;t have an account yet?{" "}
+                        <Link
+                            className="text-amber-500 underline underline-offset-2 decoration-amber-500"
+                            href={"/register"}
+                        >
+                            Register Now
+                        </Link>
+                    </p>
                 </div>
             </div>
         </section>
